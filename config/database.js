7c7152baa081f9ab9c5c4c5a82b3a9719e8e4404
phanhuudaond1973-afk <phanhuudaond1973@gmail.com>
2026-04-03 18:19:12 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,12 +1,8 @@
 //Mã nguồn được chia sẻ youtube SOURCE ALL
 module.exports = {
-	'url': 'mongodb://127.0.0.1:27017',
-	'options': {
-		'dbName': 'rikfan', //
-		//'dbName': 'GAME', //
-		//'dbName': 'admin', //
-		'useNewUrlParser': true,
-		'useUnifiedTopology': true,
-		//'autoIndex':       false,
-	},
+  url: process.env.MONGO_URL,
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
 };
